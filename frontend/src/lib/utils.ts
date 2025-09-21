7b90585a539b32bfdea9e6f3ba2bac9b9ef7c191
KokoBrian <kodenova.tech@gmail.com>
2025-09-21 18:45:13 +0300

    add modules

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,5 @@
-mport { clsx, type ClassValue } from "clsx";
+import { clsx, type ClassValue } from "clsx";
+
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
